740817a8977f17f85fd8b50f331dcafe9386111d
Vinay <vinay.chaubey@algoworks.com>
2020-08-27 22:53:12 +0530

    all files push

--- imdb_task/wsgi.py
+++ imdb_task/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imdb_task.settings')
-
-application = get_wsgi_application()
+application = get_asgi_application()
+application = DjangoWhiteNoise(application)
